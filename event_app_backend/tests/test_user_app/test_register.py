e3a8ac4c78b91c5d7dba38b6c8a43ca73da192df
Azeem <azeemamjad225@gmail.com>
2025-07-25 13:10:13 +0500

    Adding App User Test

---
+++ event_app_backend/tests/test_user_app/test_register.py
@@ -0,0 +1,29 @@
+import pytest
+from rest_framework import status
+from user_app.models import AppUser
+
+@pytest.mark.django_db
+def test_register_user(client):
+    url = 'http://localhost:8000/api/user/register/'
+    assert AppUser.objects.all().exists() == False
+    response = client.post(url, {"username": "admin", "password": "admin"})
+    assert AppUser.objects.all().exists() == True
+    assert response.json()['message'] == 'User registered successfully'
+    assert response.status_code == status.HTTP_200_OK
+
+@pytest.mark.django_db
+def test_register_user_without_body(client):
+    url = 'http://localhost:8000/api/user/register/'
+    response = client.post(url)
+    assert AppUser.objects.all().exists()
+    assert response.json()['username'][0] == response.json()['password'][0] == 'This field is required.'
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+@pytest.mark.django_db
+def test_register_with_duplicates(client):
+    url = 'http://localhost:8000/api/user/register/'
+    client.post(url, {"username": "admin", "password": "admin"})
+    response = client.post(url, {"username": "admin", "password": "admin"})
+    assert response.json()['username'][0] == 'A user with that username already exists.'
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    
